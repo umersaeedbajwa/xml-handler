a9ec41847ae72749ed6a607b75f3d0216b52bb14
jz <umersaeedbajwa@gmail.com>
2025-09-20 13:11:22 +0500

    add scripts

---
+++ scripts/functions/file_exists.lua
@@ -0,0 +1,7 @@
+--check if a file exists
+	function file_exists(name)
+		local f = io.open(name, "r")
+		if not f then return end
+		f:close()
+		return name
+	end
---
+++ scripts/functions/trim.lua
@@ -0,0 +1,6 @@
+--add the trim function
+	function trim(s)
+		if (s) then
+			return s:gsub("^%s+", ""):gsub("%s+$", "")
+		end
+	end
