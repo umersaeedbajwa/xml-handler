bc1b1865effa4786066160ae5211f083ab2dcf81
jz <umersaeedbajwa@gmail.com>
2025-09-26 01:29:16 +0500

    encoder decoder debug

--- scripts/functions/lunajson.lua
+++ scripts/functions/lunajson.lua
@@ -1,6 +1,6 @@
-local newdecoder = require 'resources.functions.lunajson.decoder'
-local newencoder = require 'resources.functions.lunajson.encoder'
-local sax = require 'resources.functions.lunajson.sax'
+local newdecoder = require 'functions.lunajson.decoder'
+local newencoder = require 'functions.lunajson.encoder'
+local sax = require 'functions.lunajson.sax'
 -- If you need multiple contexts of decoder and/or encoder,
 -- you can require lunajson.decoder and/or lunajson.encoder directly.
 return {
